5ec9a227431be31b518d8a022ddda112300ef31b
Lucasdias79931 <lucasdias26@hotmail.com>
2024-10-17 23:04:32 -0300

    getcoordenadas

--- prepareToAling.py
+++ prepareToAling.py
@@ -1,30 +1,65 @@
 import os
+import re
 
 class PrepareToAling:
     def __init__(self) -> None:
+        # Dicionário para armazenar a sequência
         self.sequence = dict()
+        # Lista para armazenar as coordenadas
+        self.coordenadas = list()
 
     def getSequence(self, directory: str) -> None:
 
         try:
             with open(directory, "r") as file:
-                header = ""
-                seq = ""
-                for line in file:
-                    if not line.startswith(">"):
-                        seq += line.split("\n")[0]
-                    else:
-                        header = line
+                sequence_name = ""
+                sequence = ""
 
-                if header and seq:
-                    self.sequence[header] = seq
+               
+                for line in file:
+                    if not line.startswith('>'):  
+                        sequence += line.strip() 
+                        
+                    else:
+                        if sequence_name and sequence: 
+                        
+                            self.sequence[sequence_name] = sequence
+                        sequence_name = line.strip().replace('>', '')  # Remove o '>'
+                        sequence = ""  # Reseta a sequência
+
+                # Adiciona a última sequência após o loop
+                if sequence_name and sequence:
+                    self.sequence[sequence_name] = sequence
+                    
             
         except FileNotFoundError as e:
             print("Erro: "+ str(e))
-            return None
+            
         except PermissionError as e:
             print("Erro: "+ str(e))
-            return None
+            
+    def getCoordenadas(self) -> None:
+        # Inicializa a lista de coordenadas
+        coordenadas = []
+
+        # Itera sobre os cabeçalhos do dicionário de sequências
+        for header in self.sequence.keys():
+            # Ajusta a regex para capturar a letra (se houver) junto com o número
+            padrao = r"([a-zA-Z]?\d{6,7})-(\d{6,7})"
+
+            # Usando re.findall para encontrar todas as correspondências
+            resultados = re.findall(padrao, header)
+
+            if resultados:
+                # Adiciona as coordenadas encontradas na lista 'coordenadas'
+                for match in resultados:
+                    coordenadas.append(match)
+
+        # Armazena as coordenadas no atributo da classe
+        self.coordenadas = coordenadas
+
+        print("Coordenadas obtidas com sucesso!")
+        
 
 
 
@@ -37,4 +72,5 @@ for inter in range(1, 39):
     print(f"Preparando o arquivo: {directory}")
     prepare = PrepareToAling()
     prepare.getSequence(directory)
-    print(prepare.sequence)
+    
+   
