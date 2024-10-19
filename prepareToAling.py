f04ccd429e2b6a4695b2b1062b9b40e1b5186a81
Lucasdias79931 <lucasdias26@hotmail.com>
2024-10-19 00:03:58 -0300

    metodo 'getCoordenadas' corrigindo

--- prepareToAling.py
+++ prepareToAling.py
@@ -41,23 +41,22 @@ class PrepareToAling:
     def getCoordenadas(self) -> None:
         # Inicializa a lista de coordenadas
         coordenadas = []
-
+    
         # Itera sobre os cabeçalhos do dicionário de sequências
         for header in self.sequence.keys():
-            # Ajusta a regex para capturar a letra (se houver) junto com o número
-            padrao = r"([a-zA-Z]?\d{6,7})-(\d{6,7})"
-
+            # Ajusta a regex para capturar as coordenadas
+            padrao = r"([cC]?\d+)-(\d+)"
+    
             # Usando re.findall para encontrar todas as correspondências
             resultados = re.findall(padrao, header)
-
+    
             if resultados:
                 # Adiciona as coordenadas encontradas na lista 'coordenadas'
-                for match in resultados:
-                    coordenadas.append(match)
-
+                coordenadas.extend([list(resultado) for resultado in resultados])
+    
         # Armazena as coordenadas no atributo da classe
         self.coordenadas = coordenadas
-
+    
         print("Coordenadas obtidas com sucesso!")
         
 
@@ -72,5 +71,8 @@ for inter in range(1, 39):
     print(f"Preparando o arquivo: {directory}")
     prepare = PrepareToAling()
     prepare.getSequence(directory)
+    prepare.getCoordenadas()
+    print(prepare.coordenadas)
     
-   
+    
+ 
