81acd8f9a22090dad920956e27f3b5c92b748eb3
Lucasdias79931 <lucasdias26@hotmail.com>
2024-11-05 16:18:25 -0300

    getCodingPositions

--- nonCoding.py
+++ nonCoding.py
@@ -3,10 +3,15 @@ import re
 
 class NonCoding:
     def __init__(self) -> None:
+        #sequência completa
         self.sequence = ""
+        #tamanho da sequência
         self.length = 0
-        self.codingPositions= list()
-    
+        #Regiões codificantes
+        self.codingPositions = list()
+        #Regiões ncodificantes
+        self.nocodingPositions = list()
+        
 
     # obtém a sequência completa
     def get_sequence(self, genome_name):
@@ -23,12 +28,39 @@ class NonCoding:
         except Exception as e:
             print(f"Erro inesperado: {e}")
 
+    # Busca a região codificante 
+    def getCodingPosition(self, file_name):
+        try:
+            headers = []
+            with open(file_name, "r") as file:
+                for line in file:
+                    if line.startswith(">"):
+                        header = line.strip()
+                        
+                        padrao = r"([cC]?\d+)-(\d+)"
+    
+                
+                        resultados = re.findall(padrao, header)
+                        headers.extend(resultados)
+                    
+            self.codingPositions.extend(headers)
+        except FileExistsError as e:
+            print("ER :" + e)
+        except FileNotFoundError as e:
+            print("Error:" + e)
+    
+
+
+
 ################# executar ############################
 if __name__ == "__main__":
     nonCoding = NonCoding()
     nonCoding.get_sequence("chromosome1HomoSapien/sequence.fasta")
 
-    print(f"length of sequence:{nonCoding.length}")
+    nonCoding.getCodingPosition("CDSsPreparadosParaAlinhar/cds2.fasta")
+
+    nonCoding.getCodingPosition("CDSsPreparadosParaAlinhar/cds3.fasta")
+    print(f"codingPositions:{nonCoding.codingPositions}")
 
             
         
