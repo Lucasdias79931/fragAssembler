1b643b0aebf26a3820518e2663453ccaec5964fa
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-11-18 16:17:00 -0300

    atualizado

--- AlingCDSs.py
+++ AlingCDSs.py
@@ -1,22 +1,58 @@
-from Aling import Align, Minimap2
+import subprocess
 import time
 import os
 
+class Minimap2:
+    def __init__(self) -> None:
+        pass
 
-base_directory = os.getcwd()
+    @staticmethod
+    def align(reference_file, sequence_file, output_file):
+        command = [
+            "minimap2",
+            "-a",
+            reference_file,
+            sequence_file
+        ]
+
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        try:
+            with open(output_file, "w") as out:
+                subprocess.run(command, stdout=out, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Erro ao executar o Minimap2: {e.stderr.decode('utf-8')}")
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+
+
+########################### executar ############################
+here = os.path.dirname(os.path.abspath(__file__))
 
 
 
 # Alinhar com Minimap2
 start_timeWithMinimap2 = time.time()
-for i in range(1, 13):
-    reference_fasta = os.path.join(base_directory, f"chromosome1HomoSapien/reversedSequence.fasta")
-    fragments_fasta = os.path.join(base_directory, f"CDSsPreparadosC/cds{i}.fasta")
-    seqs_alinhadas = os.path.join(base_directory, f"CDSsComComplementosAlinhados/cds{i}Alinhado.sam")
 
-    aligner = Minimap2(reference_fasta, fragments_fasta, seqs_alinhadas)
-    align = Align(aligner)
-    align.perform_alignment()
+referenceSequence = os.path.join(here, "chromosome1HomoSapien/sequence.fasta")
+referenceSequenceReversed = os.path.join(here, "chromosome1HomoSapien/reversedSequence.fasta")
+cds = os.path.join(here, "CDSsPreparados/cds.fasta")
+cdsInC = os.path.join(here, "CDSsPreparados/cdsInC.fasta")
+nonCoding = os.path.join(here, "NonCds/nonCds.fasta")
+nonCodingInC = os.path.join(here, "NonCds/nonCdsInC.fasta")
+
+os.makedirs(os.path.join(here, "Alinhados"), exist_ok=True)
+
+destineCds = os.path.join(here, "Alinhados/cdsAling.fasta")
+destineCdsInC = os.path.join(here, "Alinhados/cdsInCAling.fasta")
+destineNonCds = os.path.join(here, "Alinhados/nonCdsAling.fasta")
+destineNonCdsInC = os.path.join(here, "Alinhados/nonCdsInCAling.fasta")
+
+Minimap2.align(referenceSequence, cds, destineCds)
+Minimap2.align(referenceSequenceReversed, cdsInC, destineCdsInC)
+Minimap2.align(referenceSequence, nonCoding, destineNonCds)
+Minimap2.align(referenceSequenceReversed, nonCodingInC, destineNonCdsInC)
+
+
 
 elapsed_timeWithMinimap2  = time.time() - start_timeWithMinimap2 
 
